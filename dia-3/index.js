536ddcaae903ea2b98bfa5d9740d5c3402494982
Bruno Nunes <bnsb1997@gmail.com>
2024-08-04 18:16:18 -0300

    criando a pasta do dia 3

---
+++ dia-3/index.js
@@ -0,0 +1,8 @@
+let nome = prompt("Digite o seu nome");
+let idade = parseInt(prompt("Digite a sua idade"));
+let altura = Number(prompt("Digite a sua altura"));
+let peso = parseInt(prompt("Digite o seu peso"));
+let anoNasc = prompt("Digite o Ano Atual") - idade;
+let imc = peso/(altura*altura);
+
+console.log('Olá' + nome + ', você tem' + idade + ' anos, nasceu em ' + anoNasc + ', tem' + altura + 'de altura, pesa' + peso + 'kg e seu IMC é:' + imc + 'kg/m2.')
