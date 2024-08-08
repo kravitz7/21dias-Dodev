244014852e06ea9ab56da021110f84df19013382
Bruno Nunes <bnsb1997@gmail.com>
2024-08-08 10:01:24 -0300

    arquivo de imagem adicionado no dia-3

--- dia-3/index.js
+++ dia-3/index.js
@@ -5,4 +5,5 @@ let peso = parseInt(prompt("Digite o seu peso"));
 let anoNasc = prompt("Digite o Ano Atual") - idade;
 let imc = peso/(altura*altura);
 
-console.log('Olá' + nome + ', você tem' + idade + ' anos, nasceu em ' + anoNasc + ', tem' + altura + 'de altura, pesa' + peso + 'kg e seu IMC é:' + imc + 'kg/m2.')
+console.log('Olá ' + nome + ', você tem ' + idade + ' anos, nasceu em ' + anoNasc + ', tem ' + altura + ' de altura, pesa ' + peso + ' kg e seu IMC é: ' + imc + 'kg/m2.')
+
